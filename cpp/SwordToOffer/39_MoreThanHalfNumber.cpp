470d82e1de67f11beee3aab986bf02da6fe29958
lyonling <935257580@qq.com>
2019-09-20 19:28:01 +0800

    add solutions

---
+++ cpp/SwordToOffer/39_MoreThanHalfNumber.cpp
@@ -0,0 +1,34 @@
+// 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+// 例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。
+// 由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int MoreThanHalfNum_Solution(vector<int> numbers) {
+        int N = numbers.size();
+        if(N <= 0)
+            return 0;
+        int res = numbers[0];
+        int count = 1;
+
+        for(int i=1; i<N; i++) {
+            if(count == 0){
+                res = numbers[i];
+            }
+            count += (numbers[i] == res ? 1:-1);
+        }
+        count = 0;
+        for(auto x: numbers){
+            if(x == res)
+                count++;
+        }
+        if(count > (N/2))
+            return res;
+        else
+            return 0;
+    }
+};
---
+++ cpp/SwordToOffer/40_KLeastNumber.cpp
@@ -0,0 +1,42 @@
+// 输入n个整数，找出其中最小的K个数。
+// 例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+
+#include<iostream>
+#include<vector>
+#include<multiset>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
+
+        // // Solution 1: O(nlogn)
+        // vector<int> res;
+        // if(input.empty()||k>input.size())
+        //     return res;
+        
+        // sort(input.begin(), input.end());
+        // for(int i=0; i<k; i++){
+        //     res.push_back(input[i]);
+        // }
+        // return res;
+
+        vector<int> res;
+        if(input.empty() || k < 1 || input.size() < k)
+            return res;
+        
+        multiset<int, greater<int> > least_nums;
+        for(vector<int>::iterator iter = input.begin(); iter<input.end(); iter++){
+            if(least_nums.size() < k)
+                least_nums.insert(*iter);
+            else {
+                int max_num = *least_nums.begin();
+                if(*iter < max_num){
+                    least_nums.erase(least_nums.begin());
+                    least_nums.insert(*iter);
+                }
+            }
+        }
+        return vector<int>(least_nums.begin(), least_nums.end());
+    }
+};
