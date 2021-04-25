c072debf4ef4bd3e430934b180ed0d86b7c34959
Ling, Liyang <liyang.ling@intel.com>
2021-04-26 01:17:38 +0800

    add quetion 65

---
+++ cpp/SwordToOffer/65_addWithoutArithmetic.cpp
@@ -0,0 +1,15 @@
+// 写一个函数，求两个整数之和，要求在函数体内不得使用 “+”、“-”、“*”、“/”
+// 四则运算符号。
+
+class Solution {
+public:
+  int add(int a, int b) {
+    while (b != 0) {
+      auto temp = a ^ b;
+      // c++不支持负数移位, 所以先强制类型转换为无符号数(unsigned int)
+      b = static_cast<unsigned>(a & b) << 1;
+      a = temp;
+    }
+    return a;
+  }
+};
