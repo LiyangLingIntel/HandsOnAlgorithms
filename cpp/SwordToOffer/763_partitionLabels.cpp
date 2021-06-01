ddae6395edb62f1aadf6c285f2f799096760b710
Ling, Liyang <liyang.ling@intel.com>
2021-06-02 01:20:01 +0800

    add question 763

---
+++ cpp/SwordToOffer/763_partitionLabels.cpp
@@ -0,0 +1,24 @@
+// 字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，
+// 同一字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。
+
+#include "00_commonHeader.hpp"
+
+class Solution {
+  public:
+    vector<int> partitionLabels(string s) {
+        vector<int> lastPos(s.size(), 0);
+        for (int i = 0; i < s.size(); i++) {
+            lastPos[s[i] - 'a'] = i;
+        }
+        int start = 0, end = 0;
+        vector<int> partition;
+        for (int i = 0; i < s.size(); i++) {
+            end = max(end, lastPos[s[i] - 'a']);
+            if (end == i) {
+                partition.push_back(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return partition;
+    }
+};
