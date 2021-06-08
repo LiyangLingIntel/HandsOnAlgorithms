bb5c5546208492a18f2692d9ed2257e0759f8291
Ling, Liyang <liyang.ling@intel.com>
2021-06-09 00:59:57 +0800

    update STO Q07

--- cpp/SwordToOffer/07_rebuildBinaryTree.cpp
+++ cpp/SwordToOffer/07_rebuildBinaryTree.cpp
@@ -20,24 +20,24 @@
 
 // 0 <= 节点个数 <= 5000
 
-
 #include "00_commonHeader.hpp"
 
 // Definition for a binary tree node.
 struct TreeNode {
     int val;
-    TreeNode* left;
-    TreeNode* right;
+    TreeNode *left;
+    TreeNode *right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 class Solution {
-public:
-
-    TreeNode* buildSubTree(vector<int>& preorder, int pre_s, int pre_e, vector<int>& inorder, int in_s, int in_e) {
-        if (pre_s > pre_e || in_s > in_e) return nullptr;
+  public:
+    TreeNode *buildSubTree(vector<int> &preorder, int pre_s, int pre_e,
+                           vector<int> &inorder, int in_s, int in_e) {
+        if (pre_s > pre_e || in_s > in_e)
+            return nullptr;
 
         int root_val = preorder[pre_s];
-        TreeNode* root = new TreeNode(root_val);
+        TreeNode *root = new TreeNode(root_val);
 
         int root_id;
         for (int i = in_s; i <= in_e; i++) {
@@ -48,17 +48,17 @@ public:
         }
         int left_size = root_id - in_s;
 
-        root->left = buildSubTree(preorder, pre_s + 1, pre_s + left_size, inorder,
-                                in_s, root_id - 1);
-        root->right = buildSubTree(preorder, pre_s + left_size + 1, pre_e, inorder,
-                                root_id + 1, in_e);
+        root->left = buildSubTree(preorder, pre_s + 1, pre_s + left_size,
+                                  inorder, in_s, root_id - 1);
+        root->right = buildSubTree(preorder, pre_s + left_size + 1, pre_e,
+                                   inorder, root_id + 1, in_e);
 
-        return root;       
+        return root;
     }
 
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
         int n = preorder.size();
-        TreeNode* res = buildSubTree(preorder, 0, n-1, inorder, 0, n-1);
+        TreeNode *res = buildSubTree(preorder, 0, n - 1, inorder, 0, n - 1);
         return res;
     }
 };
